78917e147e93d982ddb7c68f4f5ef862be379a57
MArtin <martinsko@fr.fr>
2021-08-24 18:30:44 +0200

    update date project

--- js/projects_data.js
+++ js/projects_data.js
@@ -2,7 +2,7 @@ let projectsData = {
     "moonstone": {
         "name": "Moonstone Kingdom",
         "url": "https://github.com/Adarshmihir/Moonstone.git",
-        "subname": "University project",
+        "subname": "University project (2021)",
         "description": "The Moonstone Kingdom turns out to be a 3D RPG game with an open world. " + 
             "It is part of a dynamic of main and secondary quests in which the player evolves at the same time as his world." +
             "Alongside the RPG combat mechanics and the quests given by the NPCs, the game also offers a city management " + 
@@ -12,7 +12,7 @@ let projectsData = {
     "gladiaball": {
         "name": "Gladiaball",
         "url": "https://mega.nz/file/1JNQFRyI#c8QfY0GVfYIBSPZ6uoY-mLszhxYGVB7tL1ijnk2qqPQ",
-        "subname": "University project",
+        "subname": "University project (2019-2020)",
         "description": "The game takes place in a mythological universe and takes up the basics of gladiatorial combat in the Roman Empire. " + 
             "The matches take place in a coliseum where the player fights against deities. " + 
             "The player is alone and carries a weapon similar to a baseball bat. The main character, must hit a ball in order " + 
@@ -23,7 +23,7 @@ let projectsData = {
     "blowbug": {
         "name": "Blow The Bug",
         "url": "https://mega.nz/file/UMdiGJzL#lNOJtTV6QUkJcFZjXWXFeVol3gS6ZGpQbA7CQjZgPno",
-        "subname": "International GameJam",
+        "subname": "International GameJam (2020)",
         "description": "This game was created during an international GameJam between my University of Chicoutimi and the University of Grenoble." +
             "Our game was inspired by the Japanese TV game show \"cockroach game\". The goal is simple, blow as hard as you can into a pipe in order" +
             "to send the insect into your opponent's mouth. In addition to ending up with a cockroach in her mouth, the loser must also eat it." + 
@@ -34,7 +34,7 @@ let projectsData = {
     "purge": {
         "name": "The Purge",
         "url": "https://yuukini.itch.io/purge",
-        "subname": "WonderJam",
+        "subname": "WonderJam 2020",
         "description": "This game was created during WonderJam (GameJam organized by UQAC and Ubisoft Saguenay)." +
             "the imposed theme was \"frenzy\", we decided to create an arena in which a berserker has to exterminate hordes of enemies," +
             "at each of his attacks he increases his frenzy, which allows him to passively increase his life steal. " +
@@ -53,7 +53,7 @@ let projectsData = {
     "CHANGE": {
         "name": "IT department",
         "url": "",
-        "subname": "Create and update the computerised patient file",
+        "subname": "Create and update the computerised patient file (2018-Now)",
         "description": " As part of my Computer Science DUT, I had the opportunity to do an internship at the end of my training, and since then I have occasionally returned to work there to maintain and improve my work.</br>" +
             "The integration of a contextual call to display the new patient record software (Axigate) in the old DPI tool (Orbis of the AGFA group)," +
             "for a transition phase where the two software cohabit.</br>- The construction, thanks to a mock-up, of a dashboard of the medical units of the CHANGE." +
